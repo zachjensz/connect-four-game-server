0dc5ec94ee851f0120afe6dda61aaf5b97093119
nightness <joshguyette@yahoo.com>
2021-08-17 14:35:49 -0500

    refactoring

--- main.js
+++ main.js
@@ -3,31 +3,30 @@ const PORT = 5000
 const io = require("socket.io")(PORT, {
   cors: {
     //origin: ['http://localhost:3000', 'http://connectfourgame.com:80']
-    origin: "*",
+    origin: '*', // Yikes!!!
   },
 })
+
+io.on("connection", (socket) => addClient(socket))
 
 function addClient(socket) {
   const eventHandlers = []
   clientSockets.push(socket)
 
-  console.log(socket.id + " has joined")
+  console.log(`${socket.id} has joined`)
 
   eventHandlers.push(['disconnect', () => {
-    console.log(socket.id + " has left")
+    console.log(`${socket.id} has left`)
+  }])
+
+  eventHandlers.push(['find-opponent', () => {
+    console.log(`${socket.id} is looking for an opponent`)
   }])
 
   eventHandlers.push(['drop',  (column) => {
-    console.log("drop: ", column)
+    console.log(`${socket.id} drops in column ${column}`)
     socket.broadcast.emit("drop", column)
   }])
 
-  return {
-    eventHandlers,
-  }
-}
-
-io.on("connection", (socket) => {
-  const { eventHandlers } = addClient(socket)
   eventHandlers.forEach(([name, handler]) => socket.on(name, handler))
-})
+}
