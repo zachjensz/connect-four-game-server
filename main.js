5e3cda9695fa00dd367d1493ab4c047b873672b0
Zach <zachjensz@protonmail.com>
2021-08-17 14:51:01 +1000

    Minor fixes

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
 const PORT = 3500
 
 const io = require('socket.io')(PORT, {
-    cors: {
-        origin: [
-            'http://localhost:3000',
-            'http://connectyfourgame.com:80'
-        ]
-    }
+  cors: {
+    origin: ['http://localhost:3000', 'http://connectyfourgame.com:80']
+  }
 })
 
-io.on('connection', socket => {
-    console.log(socket.id)
-    socket.on("drop", () => {
-                
-    })
+io.on('connection', (socket) => {
+  console.log(socket.id)
+  socket.on('drop', () => {})
 })
