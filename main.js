fd70161db77c8fd5e8e0af6d54db33fddca4d770
nightness <joshguyette@yahoo.com>
2021-08-16 23:45:39 -0500

    snapshot

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+const PORT = 3500
+
+const io = require('socket.io')(PORT, {
+    cors: {
+        origin: [
+            'http://localhost:3000',
+            'http://connectyfourgame.com:80'
+        ]
+    }
+})
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.on("drop", () => {
+                
+    })
+})
