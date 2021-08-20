b90ec63ecd26248f0fb038b2a2f0df747b6b82d6
nightness <joshguyette@yahoo.com>
2021-08-20 14:09:17 -0500

    server is functional now

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
+const fs = require("fs")
+const socket = require("socket.io")
+
+const setSavedState = (state) => {
+  fs.writeFile("server.db", JSON.stringify(state), function (err) {
+    if (err) {
+      return console.error(err)
+    }
+    console.log("Server state written successfully!")
+  })
+}
+
+const getSavedState = () => {
+  fs.readFile("server.db", function (err, data) {
+    if (err) {
+      return console.error(err)
+    }
+    return JSON.parse(data)
+  })
+}
+
+let dbData = getSavedState() ?? {
+  test: 'Hello DB'
+}
+dbData['save'] = () => setSavedState(dbData)
+dbData.save()
+
 // Setup server on port 5000 and listen for connections
 const PORT = 5000
-const io = require("socket.io")(PORT, {
+const io = socket(PORT, {
   cors: {
     //origin: ['http://localhost:3000', 'http://connectfourgame.com:80']
-    origin: "*", // Yikes!!!
+   origin: "*", // Yikes!!!
   },
 })
 io.on("connection", (socket) => addClient(socket))
 
 // All of the sockets for all of the clients
 const clientSockets = []
-const getSocketById = (id) => clientSockets.find(socket => socket.id === id)
+const getSocketById = (id) => clientSockets.find((socket) => socket.id === id)
+const removeSocketById = (id) => clientSockets.filter((ele) => ele.id != socket.id)
 
 // Games in-progress
 const gamesBeingPlayed = []
 
+function removeFromLookingFor(socketId) {
+  lookingForOpponents =
+    lookingForOpponents.filter((ele) => ele != socketId)
+}
+
 // The socket.id's of players looking for opponents
-const lookingForOpponents = []
+let lookingForOpponents = []
 function addClient(socket) {
   const emitHandlers = []
   clientSockets.push(socket)
@@ -28,6 +61,8 @@ function addClient(socket) {
     "disconnect",
     () => {
       console.log(`${socket.id} has left`)
+      removeSocketById(socket.id)
+      removeFromLookingFor(socket.id)
     },
   ])
 
@@ -45,13 +80,19 @@ function addClient(socket) {
         console.log(`${socket.id} is waiting for an opponent`)
         return
       }
+
       const opponentSocket = getSocketById(opponentId)
-      socket.emit('opponent-found', opponentId)
-      opponentSocket.emit('opponent-found', socket.id)
+      removeFromLookingFor(socket.id)      
+      removeFromLookingFor(opponentSocket.id)
+      // Emit to both players that an opponent was found
+      const startingPlayer = randomPlayer()
+      socket.emit("opponent-found", { id: opponentId, startingPlayer: !!startingPlayer })
+      opponentSocket.emit("opponent-found", {id: socket.id, startingPlayer: !startingPlayer })
       gamesBeingPlayed.push({
-        startingPlayer: randomPlayer(),
+        startingPlayer,
         opponentId,
-        opponentSocket
+        opponentSocket,
+        socket
       })
       console.log(`${socket.id} is playing a game with ${opponentId}`)
     },
@@ -71,3 +112,4 @@ function addClient(socket) {
 }
 
 const randomPlayer = () => Math.floor(Math.random() * 2)
+
